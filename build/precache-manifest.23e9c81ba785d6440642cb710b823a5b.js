bc05312a31a33d4d271f7a0ae2827f1ef5d8c481
SASWAT123 <psaswat21@gmail.com>
2020-06-16 01:14:42 +0530

    added _redirects

---
+++ build/precache-manifest.23e9c81ba785d6440642cb710b823a5b.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11c4db8903cf50c153c89bf875483554",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d3a2869b3bbb757890ed",
+    "url": "/static/css/2.42e16ef6.chunk.css"
+  },
+  {
+    "revision": "68830e850db31a77f471",
+    "url": "/static/css/main.e46246a6.chunk.css"
+  },
+  {
+    "revision": "d3a2869b3bbb757890ed",
+    "url": "/static/js/2.a2ec87e5.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.a2ec87e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68830e850db31a77f471",
+    "url": "/static/js/main.82343524.chunk.js"
+  },
+  {
+    "revision": "30ef98ccc078dcdc8699",
+    "url": "/static/js/runtime-main.13135219.js"
+  },
+  {
+    "revision": "53e01e7d87bde447567ccae460990e4f",
+    "url": "/static/media/Dialogflow.53e01e7d.png"
+  },
+  {
+    "revision": "7a151e21e5d49fc167d998b4ce8e1bda",
+    "url": "/static/media/Saswat.7a151e21.jpg"
+  },
+  {
+    "revision": "553b88f7c39373a32aeae66b88cab8e1",
+    "url": "/static/media/URL-shortener.553b88f7.png"
+  },
+  {
+    "revision": "5e7fb1ed4b2f62d160b914e9d058afee",
+    "url": "/static/media/customer-churn.5e7fb1ed.png"
+  },
+  {
+    "revision": "e248591284e00b117da6cc644b3438f6",
+    "url": "/static/media/employee.e2485912.png"
+  },
+  {
+    "revision": "386ebdd57f6e3c8c06a5cf47b0fc8f59",
+    "url": "/static/media/flutter_chat.386ebdd5.jpg"
+  },
+  {
+    "revision": "0627258934cb99ee96f430558e3f028c",
+    "url": "/static/media/microservices.06272589.png"
+  },
+  {
+    "revision": "a8fd73b0e2d0f3b5a921b9375f3abf0c",
+    "url": "/static/media/portfolio.a8fd73b0.png"
+  },
+  {
+    "revision": "e5eb4116f81180b662f6864c91918808",
+    "url": "/static/media/saswat_full.e5eb4116.jpg"
+  },
+  {
+    "revision": "b92c4233769decdf8e0711c87fdd556e",
+    "url": "/static/media/slack_clone.b92c4233.png"
+  },
+  {
+    "revision": "c6fc4954dc24cd278b087cd1a83e9856",
+    "url": "/static/media/springsecurity.c6fc4954.jpg"
+  },
+  {
+    "revision": "99a5123aa53f3d2d40c339d9f21f4adb",
+    "url": "/static/media/webmap.99a5123a.png"
+  }
+]);
