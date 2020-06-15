a5fab831af48291b4ccdd5b300103834448a132d
SASWAT123 <psaswat21@gmail.com>
2020-06-15 16:11:16 +0530

    Added more projects

--- src/components.js/projects.js
+++ src/components.js/projects.js
@@ -4,6 +4,10 @@ import cutomer_churn from "../assets/customer-churn.png";
 import flutter_chat from "../assets/flutter_chat.jpg";
 import portfolio from "../assets/portfolio.png";
 import webmap from "../assets/webmap.png";
+import slack_clone from "../assets/slack_clone.png";
+import microservices from "../assets/microservices.png";
+import url_shortner from "../assets/URL-shortener.png";
+import Dialogflow from "../assets/Dialogflow.png";
 import { Row, Col } from "reactstrap";
 
 class Projects extends Component {
@@ -16,6 +20,7 @@ class Projects extends Component {
         description:
           "Rehabilitation android application with integrated paypal payment gateway. We used DialogFlow (formerly api.ai) for implementing the chatbot in this. Firebase is used for backend.",
         link: "https://github.com/garvitkhamesra/Amigo",
+        src: Dialogflow,
       },
       {
         title: "Portfolio",
@@ -27,7 +32,7 @@ class Projects extends Component {
       },
       {
         title: "Peer to Peer Chat",
-        technology: "Flutter",
+        technology: "Flutter + Firebase",
         description:
           "This is a peer to peer chat application developed using Flutter and Firebase for managed backend services. It works in real time and you can also see the chat history of their recent chats.",
         link:
@@ -57,6 +62,30 @@ class Projects extends Component {
           "This is a full stack web application that is developed using Angular 2, Springboot and MongoDb as part of my full stack bootcamp. This performs all the basic CRUD operations.",
         link: "https://github.com/SASWAT123/Full-Stack-Project",
       },
+      {
+        title: "URL Shortner",
+        technology: "React + Express + Mongo Application",
+        description:
+          "This is a simple application that takes in a long URL and converts it into a small URL. This can be extended to build some real world application that involves shortening of urls.",
+        link: "https://github.com/SASWAT123/URLShortener",
+        src: url_shortner,
+      },
+      {
+        title: "Slack Clone",
+        technology: "React + Redux + Firebase",
+        description:
+          "This is a clone of the slack app and its functionality. I am using React semantic UI for this application. Redux is used for the global state management of the application.",
+        link: "https://github.com/SASWAT123/Slack-Clone",
+        src: slack_clone,
+      },
+      {
+        title: "Springboot Microservices",
+        technology: "Springboot + Eureka Microservices",
+        description:
+          "This is a simple application demonstrating the implementation of microservices with Eureka. I am implemenitng Eureka service discovery and registry along with Hysterix for fault tolerance. ",
+        link: "https://github.com/SASWAT123/Microservices-Architecture",
+        src: microservices,
+      },
     ];
   }
   render() {
@@ -67,7 +96,7 @@ class Projects extends Component {
             <CardView cardDetails={this.state[4]} />
           </Col>
           <Col xs="12" sm="6" md="4" lg="4">
-            <CardView cardDetails={this.state[0]} />
+            <CardView cardDetails={this.state[7]} />
           </Col>
           <Col xs="12" sm="6" md="4" lg="4">
             <CardView cardDetails={this.state[1]} />
@@ -75,13 +104,24 @@ class Projects extends Component {
         </Row>
         <Row>
           <Col xs="12" sm="6" md="4" lg="4">
-            <CardView cardDetails={this.state[5]} />
+            <CardView cardDetails={this.state[8]} />
           </Col>
+          <Col xs="12" sm="6" md="4" lg="4">
+            <CardView cardDetails={this.state[2]} />
+          </Col>
+          <Col xs="12" sm="6" md="4" lg="4">
+            <CardView cardDetails={this.state[6]} />
+          </Col>
+        </Row>
+        <Row>
           <Col xs="12" sm="6" md="4" lg="4">
             <CardView cardDetails={this.state[3]} />
           </Col>
           <Col xs="12" sm="6" md="4" lg="4">
-            <CardView cardDetails={this.state[2]} />
+            <CardView cardDetails={this.state[0]} />
+          </Col>
+          <Col xs="12" sm="6" md="4" lg="4">
+            <CardView cardDetails={this.state[5]} />
           </Col>
         </Row>
       </div>
